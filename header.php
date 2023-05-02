125be30de3cfdf13db7dd579aaf8fe9be6022715
Patricia Elena Putri <patriciaelenaputri@gmail.com>
2023-05-02 15:15:22 +0700

    fix

--- header.php
+++ header.php
@@ -83,7 +83,7 @@ if (isset($_POST['logout'])) {
         </nav>
 
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
-            <a href="index3.html" class="brand-link">
+            <a href="index.php" class="brand-link">
                 <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">Klinik IMK</span>
             </a>
